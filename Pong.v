6b80b5010ff479ee673710ab486a94d1273c45a8
Ethan Shea <ethan.shea1@gmail.com>
2017-12-16 18:01:28 -0800

    Added a paddle and controls

--- Pong.v
+++ Pong.v
@@ -3,11 +3,14 @@
 
 module Pong(clk, vga_h_sync, vga_v_sync, vga_R, vga_G, vga_B, test, testGround, ground, buttonLeft, buttonRight);
 
-reg [1:0] cnt;
+reg [14:0] cnt;
 always @(posedge clk) cnt <= cnt + 1;
 
 wire vga_clk;
 assign vga_clk = cnt[1];
+
+wire game_clk;
+assign game_clk = cnt[14];
 
 input clk,buttonLeft,buttonRight;
 output vga_h_sync, vga_v_sync, vga_R, vga_G, vga_B, test, testGround, ground;
@@ -16,16 +19,17 @@ wire inDisplayArea;
 wire [9:0] CounterX;
 wire [8:0] CounterY;
 
-reg [8:0] location;
+reg [15:0] location;
+reg [5:0] speed;
 
 hvsync_generator syncgen(.clk(vga_clk), .vga_h_sync(vga_h_sync), .vga_v_sync(vga_v_sync), 
                             .inDisplayArea(inDisplayArea), .CounterX(CounterX), .CounterY(CounterY));
 
-wire paddle = (location > CounterX) & (location < CounterX + 96) & (CounterY[8:3] == 40);
+wire paddle = (CounterX > location[15:6]) & (CounterX < location[15:6] + 48) & (CounterY[8:3] == 56);
 									 
-wire R = CounterX[4] ^ CounterY[3];
-wire G = CounterX[6] ^ CounterY[6];
-wire B = CounterX[0] ^ CounterY[0];
+wire R = paddle; //CounterX[4] ^ CounterY[3];
+wire G = paddle; //CounterX[6] ^ CounterY[6];
+wire B = paddle;
 
 reg vga_R, vga_G, vga_B;
 always @(posedge clk)
@@ -37,6 +41,25 @@ end
 
 assign leftPressed = ~buttonLeft; // Buttons are NC
 assign rightPressed = ~buttonRight;
+
+
+always @(posedge game_clk) begin
+	if (leftPressed)  begin
+		if (speed != -1)
+			speed <= speed + 1;
+		if (location[15:6] != 640 - 48)
+			location <= location + speed[5:2];
+	end else if (rightPressed) begin
+		if (speed != -1)
+			speed <= speed + 1;
+		if (location[15:6] != 0) begin
+			location <= location - speed[5:2];
+		end
+	end else begin 
+		if (speed != 0)
+			speed <= speed - 1;
+	end
+end
 
 assign testGround = 0;
 assign test = clk;
